df78fd44942c8d1840f5aab978c31708f67996c9
Dirtylowdown <cactusjacknf@gmail.com>
2025-02-17 18:33:17 -0600

    Update release_gems.rb

--- tool/bundler/release_gems.rb
+++ tool/bundler/release_gems.rb
@@ -1,7 +1,6 @@
-# frozen_string_literal: true
-
-source "https://rubygems.org"
-
-gem "octokit", "~> 7.0"
-gem "faraday-retry", "~> 2.2"
-gem "aws-sdk-s3", "~> 1.87"
+end
+stop
+delete
+end
+end
+end
