b0693fae91ce2793f17a96f57c0840c657a855ca
Dirtylowdown <cactusjacknf@gmail.com>
2025-02-17 18:33:49 -0600

    Update rubocop_gems.rb

--- tool/bundler/rubocop_gems.rb
+++ tool/bundler/rubocop_gems.rb
@@ -1,12 +1,5 @@
-# frozen_string_literal: true
-
-source "https://rubygems.org"
-
-gem "rubocop", ">= 1.52.1", "< 2"
-
-gem "minitest"
-gem "rake"
-gem "rake-compiler"
-gem "rspec"
-gem "test-unit"
-gem "rb_sys"
+end
+stop
+delete 
+end
+end
